a6808e103cea46272b534cc33943fe3ea4225698
okie <okieeeee@proton.me>
2023-07-11 20:15:11 -0300

    Update Program.cs
    Error handling because someone asked for it

--- Program.cs
+++ Program.cs
@@ -1,40 +1,64 @@
-﻿// Credits to: https://github.com/XenocodeRCE/dnSpyDetector/tree/master, i did just rewrite it using NativeLibrary and Unsafe.  
-
-using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-class Program
+internal class Program
 {
-    static IntPtr kernel32;
-    static IntPtr ntDll;
+    private static IntPtr kernel32;
+    private static IntPtr ntDll;
 
-    unsafe static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        int hooksFoundCount = 0;
-
-        kernel32 = NativeLibrary.Load("kernel32.dll");
-        ntDll = NativeLibrary.Load("ntdll.dll");
-
-        hooksFoundCount += DetectHook(kernel32, "IsDebuggerPresent", 0xE9, "IsDebuggerPresent hook detected.");
-        hooksFoundCount += DetectHook(kernel32, "CheckRemoteDebuggerPresent", 0xE9, "CheckRemoteDebuggerPresent hook detected.");
-        hooksFoundCount += DetectHook(typeof(Debugger), "get_IsAttached", 0x33, "Debugger.IsAttached hook detected.");
-        hooksFoundCount += DetectHook(ntDll, "NtRaiseHardError", 0xE9, "NtRaiseHardError hook detected.");
-        hooksFoundCount += DetectHook(kernel32, "CloseHandle", 0xE9, "CloseHandle hook detected.");
-
-        if (hooksFoundCount == 0)
+        try
         {
-            Console.WriteLine("No hooks found!");
+            int hooksFoundCount = 0;
+
+            kernel32 = LoadLibrary("kernel32.dll");
+            ntDll = LoadLibrary("ntdll.dll");
+
+            hooksFoundCount += DetectHook(kernel32, "IsDebuggerPresent", 0xE9, "IsDebuggerPresent hook detected.");
+            hooksFoundCount += DetectHook(kernel32, "CheckRemoteDebuggerPresent", 0xE9, "CheckRemoteDebuggerPresent hook detected.");
+            hooksFoundCount += DetectHook(typeof(Debugger), "get_IsAttached", 0x33, "Debugger.IsAttached hook detected.");
+            hooksFoundCount += DetectHook(ntDll, "NtRaiseHardError", 0xE9, "NtRaiseHardError hook detected.");
+            hooksFoundCount += DetectHook(kernel32, "CloseHandle", 0xE9, "CloseHandle hook detected.");
+
+            if (hooksFoundCount == 0)
+            {
+                Console.WriteLine("No hooks found!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            FreeLibrary(kernel32);
+            FreeLibrary(ntDll);
         }
 
         Console.ReadLine();
-
-        NativeLibrary.Free(kernel32);
-        NativeLibrary.Free(ntDll);
     }
 
-    unsafe static int DetectHook(IntPtr module, string functionName, byte expectedOpcode, string hookMessage)
+    private static IntPtr LoadLibrary(string libraryName)
+    {
+        IntPtr library = NativeLibrary.Load(libraryName);
+        if (library == IntPtr.Zero)
+        {
+            throw new Exception($"Failed to load library: {libraryName}");
+        }
+        return library;
+    }
+
+    private static void FreeLibrary(IntPtr library)
+    {
+        if (library != IntPtr.Zero)
+        {
+            NativeLibrary.Free(library);
+        }
+    }
+
+    private static unsafe int DetectHook(IntPtr module, string functionName, byte expectedOpcode, string hookMessage)
     {
         IntPtr getProcAddressPtr = NativeLibrary.GetExport(kernel32, "GetProcAddress");
         var getProcAddress = Marshal.GetDelegateForFunctionPointer<GetProcAddressDelegate>(getProcAddressPtr);
@@ -51,7 +75,7 @@ class Program
         return 0;
     }
 
-    unsafe static int DetectHook(Type type, string methodName, byte expectedOpcode, string hookMessage)
+    private static unsafe int DetectHook(Type type, string methodName, byte expectedOpcode, string hookMessage)
     {
         var method = type.GetMethod(methodName);
         if (method != null)
